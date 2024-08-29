f107761a0ba76a87f7390d4da6cc537ea2f1562f
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-29 17:47:04 +0530

    Product Page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import RatingAndReviews from "./RatingAndReviews";
+import { Link } from "react-router-dom";
+
 
 const ProductDetail = () => {
   return (
@@ -129,9 +132,11 @@ const ProductDetail = () => {
                 <span class="title-font font-medium text-2xl text-gray-900">
                   $58.00
                 </span>
-                <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                <Link to={'/checkout'}>
+                <button class="flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded ml-80">
                   Button
                 </button>
+                </Link>
                 <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
@@ -149,6 +154,7 @@ const ProductDetail = () => {
           </div>
         </div>
       </section>
+      <RatingAndReviews/>
     </div>
   );
 };
